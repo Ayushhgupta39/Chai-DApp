61469b421a3c82b47a60a2d2769b0a1b45c79c22
Ayushhgupta39 <ayushgupta3902@gmail.com>
2023-04-27 23:25:05 +0530

    Base setup and Write Contract

--- contracts/Chai.sol
+++ contracts/Chai.sol
@@ -2,5 +2,31 @@
 pragma solidity ^0.8.9;
 
 contract Chai {
-    
+    struct Memo {
+        string name;
+        string message;
+        uint timeStamp;
+        address from;
+    }
+
+    Memo[] memos;
+    address payable owner;
+
+    constructor() {
+        // 1. msg.sender means the one who will deploy this smart contract(It's myself in this case)
+        // will become the owner
+        // 2. We will make the msg.sender as it is just a normal address, to make sure the address
+        // is able to receive Ether, we have to make it payable.
+        owner = payable(msg.sender);
+    }
+
+    function buyChai(string memory name, string memory message) public payable {
+        require(msg.value > 0, "Payment value should be more than 0 ETH");
+        owner.transfer(msg.value);
+        memos.push(Memo(name, message, block.timestamp, msg.sender));
+    }
+
+    function getMemos() public view returns(Memo[] memory) {
+        return memos;
+    }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,27 +1,31 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
+async function getBalances(address) {
+  const balanceBigInt = await hre.ethers.provider.getBalance(address);
+  return hre.ethers.utils.formatEther(balanceBigInt);
+}
+
+async function printBalances(addresses) {
+  let counter = 0;
+  for(const address of addresses) {
+    console.log(`Address ${counter} balance: `, await getBalances(address));
+  }
+}
+
+async function printMemos(memos) {
+  for(const memo of memos) {
+    const timeStamp = memo.timestamp;
+    const name = memo.name;
+    const from = memo.address;
+    const message = memo.message;
+    console.log(`At ${timeStamp}, name ${name}, address ${from}, message ${message}`);
+  }
+}
+
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
