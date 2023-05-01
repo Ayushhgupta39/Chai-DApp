8d8d6efa2f1172f336d6e1b83ed3b12523ad00bd
Ayushhgupta39 <ayushgupta3902@gmail.com>
2023-05-01 23:22:03 +0530

    Connect Metamask

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,42 @@
+import abi from "./contract/Chai.json";
 import { useState, useEffect } from "react";
 import { ethers } from "ethers";
-import { Box, Text } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 
 function App() {
-  return (
-    <div className="App">
-      <Box>
-        <Text>Hello, World</Text>
-      </Box>
-    </div>
-  );
+  const [state, setState] = useState({
+    provider: null,
+    signer: null,
+    contract: null,
+  });
+
+  useEffect(() => {
+    const connectWallet = async () => {
+      const contractAddress = "0xb27b54e097a9982b778b4709517eb5d83374e4b3";
+      const contractABI = abi.abi;
+      
+      try {
+        const { ethereum } = window;
+
+        if(ethereum) {
+          const accounts = await ethereum.request({ method: "eth_requestAccounts" })
+        }
+
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract( contractAddress, contractABI, signer );
+        setState({ provider, signer, contract });
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    connectWallet();
+  }, []);
+
+  console.log(state);
+  return <div className="App">
+    <Box>Hello, World!</Box>
+  </div>;
 }
 
 export default App;
